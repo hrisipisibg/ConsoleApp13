5c83c63be284b3ab6451c007e0bf9cf255aa9951
hrisipisibg <hristiqnmitevv@gmail.com>
2026-04-22 22:09:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lubo e maznichuk");
+            Console.WriteLine("mnogo");
+            Console.WriteLine("mnogi");
+
+        }
+    }
+}
