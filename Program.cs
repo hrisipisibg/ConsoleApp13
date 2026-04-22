2f8011e38eea8cbf9f4956e8e9fcd7870d06422c
hrisipisibg <hristiqnmitevv@gmail.com>
2026-04-22 22:10:26 +0300

    "Updated Console messages"

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lubo e maznichuk");
+            Console.WriteLine("mnogo");
+            Console.WriteLine("mnogi");
+            Console.WriteLine("lubo e maznichuk");
+            Console.WriteLine("mnogo");
+            Console.WriteLine("mnogi");
+
+        }
+    }
+}
